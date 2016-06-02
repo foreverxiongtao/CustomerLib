7bc14fd4e4bce1680ffd243f721483e62a3c1625
desperado <foreverxiongtao@gmail.com>
2016-06-02 16:39:39 +0800

    repair bug

---
+++ customerlib/src/main/java/com/desperado/customerlib/view/basepopup/BasePopup.java
@@ -0,0 +1,12 @@
+package com.desperado.customerlib.view.basepopup;
+
+import android.view.View;
+
+/**
+ * Created by 大灯泡 on 2016/1/14.
+ */
+public interface BasePopup {
+     View getPopupView();
+     View getAnimaView();
+
+}
